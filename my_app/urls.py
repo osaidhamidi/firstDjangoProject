125565ae71b12d075ce1eb6017cc135c4d79f3c9
Osayd <osaidhamidi@gmail.com>
2024-08-15 05:56:17 +0300

    final changes

--- my_app/urls.py
+++ my_app/urls.py
@@ -3,4 +3,20 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='main'),
+    path('budige/', views.budgie_page, name='budgie_page'),
+    path('canari/', views.canari_page, name='canari_page'),
+    path('cardinalred/', views.cardinalred_page, name='cardinalred_page'),
+    path('cocatoo/', views.cocatoo_page, name='cocatoo_page'),
+    path('coctail/', views.coctail_page, name='coctail_page'),
+    path('goldenparakeet/', views.goldenparakeet_page, name='goldenparakeet_page'),
+    path('grayparrot/', views.grayparrot_page, name='grayparrot_page'),
+    path('home/', views.home_page, name='home_page'),
+    path('lov/', views.lov_page, name='lov_page'),
+    path('macaw/', views.macaw_page, name='macaw_page'),
+    path('ganna/', views.ganna_page, name='ganna_page'),
+    path('siskin/', views.siskin_page, name='siskin_page'),
+    path('turkman/', views.turkman_page, name='turkman_page'),
+    path('yellowth/', views.yellowth_page, name='yellowth_page'),
+    path('zebra/', views.zebra_page, name='zebra_page'),
+
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,4 +1,50 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'my_app/main.html')
+
+def budgie_page(request):
+    return render(request, 'my_app/pages/Budgie.html')
+
+def canari_page(request):
+    return render(request, 'my_app/pages/canari.html')
+
+def cardinalred_page(request):
+    return render(request, 'my_app/pages/cardinalred.html')
+
+def coctail_page(request):
+    return render(request, 'my_app/pages/coctail.html')
+def cocatoo_page(request):
+    return render(request, 'my_app/pages/cocatoo.html')
+
+
+def goldenparakeet_page(request):
+    return render(request, 'my_app/pages/goldenparakeet.html')
+
+def grayparrot_page(request):
+    return render(request, 'my_app/pages/grayparrot.html')
+
+def home_page(request):
+    return render(request, 'my_app/pages/home.html')
+
+def lov_page(request):
+    return render(request, 'my_app/pages/lov.html')
+
+def macaw_page(request):
+    return render(request, 'my_app/pages/macaw.html')
+
+def ganna_page(request):
+    return render(request, 'my_app/pages/ganna.html')
+
+def siskin_page(request):
+    return render(request, 'my_app/pages/siskin.html')
+
+def turkman_page(request):
+    return render(request, 'my_app/pages/turkman.html')
+
+def yellowth_page(request):
+    return render(request, 'my_app/pages/yellowth.html')
+
+def zebra_page(request):
+    return render(request, 'my_app/pages/zebra.html')
